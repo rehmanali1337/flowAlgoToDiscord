6da50dfaddf931b8ebff9b5b192ab992b474844b
Rehman Ali <rehmanali.9442289@gmail.com>
2020-08-21 21:21:48 +0500

    Minor fix

--- DarkPool.py
+++ DarkPool.py
@@ -77,6 +77,21 @@ class DarkPool(threading.Thread):
             pass
         self.FLOW_LOGIN = True
 
+    # Remove emojies from strings
+
+    @staticmethod
+    def deEmojify(text):
+        regrex_pattern = re.compile(
+            pattern="["
+            "\U0001F600-\U0001F64F"  # emoticons
+            "\U0001F300-\U0001F5FF"  # symbols & pictographs
+            "\U0001F680-\U0001F6FF"  # transport & map symbols
+            "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+            "]+",
+            flags=re.UNICODE,
+        )
+        return regrex_pattern.sub(r"", text)
+
     async def send(self, desc):
         embd = discord.Embed(
             title="Dark Pool & Equity Blocks",
--- RealTime.py
+++ RealTime.py
@@ -82,6 +82,21 @@ class RealTime(threading.Thread):
             pass
         self.FLOW_LOGIN = True
 
+    # Remove emojies from strings
+
+    @staticmethod
+    def deEmojify(text):
+        regrex_pattern = re.compile(
+            pattern="["
+            "\U0001F600-\U0001F64F"  # emoticons
+            "\U0001F300-\U0001F5FF"  # symbols & pictographs
+            "\U0001F680-\U0001F6FF"  # transport & map symbols
+            "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+            "]+",
+            flags=re.UNICODE,
+        )
+        return regrex_pattern.sub(r"", text)
+
     async def send(self, desc, item_type):
         if item_type == "no_color":
             embd = discord.Embed(
